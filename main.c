834a386e942d041b61b1ae13f9a75da08f0787d7
Мерзликин Максим Максимович <ip915s16@awp06.csc.local>
2020-02-17 12:23:58 +0700

    Create function for getting command

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+void get_figure()
+{
+    string com;
+	cin >> com;
+	cout << com << endl;
+}
+int main()
+{
+	get_figure();    
+}
