f47457049afe9839e2ae6ef8c3daacb55ea43a52
Мерзликин Максим Максимович <ip915s16@mwp09.csc.local>
2020-02-29 10:37:00 +0700

    Redid get figure, create get number

--- main.c
+++ main.c
@@ -5,47 +5,35 @@
 #include <string.h>
 #include <vector>
 using namespace std;
-struct figure {
+struct data_figure {
     vector<string> name;
     vector<double> x;
     vector<double> y;
     vector<double> r;
     vector<double> perimetr;
     vector<double> area;
-};
-int MyAtoi(char a[])
+} figure;
+int get_num(string com, int k)
 {
-    int s = 0;
-    int al = strlen(a);
-    int k = 1;
-    for (int i = 0; i < al - 1; i++) {
-        k *= 10;
+    string num;
+    for (long unsigned int i = k; i < com.length(); i++) {
+        if (com[i] != ' ' && com[i] != ',') {
+            num += com[i];
+        }
     }
-    for (i = 0; a[i] != 0; i++) {
-        s += (a[i] - '0') * k;
-        k /= 10;
-    }
-    return s;
+    return stoi(num);
 }
 void get_figure()
 {
-    string com, num;
-    int k = 0;
-    cin >> com;
-    if
-        com.find("circle")
-        {
-            figure.name.push_back("circle");
-            k += 7;
-            for (int i = 0; i < com.length(); i++) {
-                if (com[k + 1] != ' ' || com[k + 1] != ',') {
-                    num[ ] = com[k];
-		   																		
-                }
-            }
-        }
-    cout << figure.name[0] << endl;
+    string com;
+    getline(cin, com);
+    cout << com << endl;
+    if (com.find("circle") != -1) {
+        figure.name.push_back("circle");
+        figure.x.push_back(get_num(com, 7));
+    }
 }
+
 int main()
 {
     get_figure();
