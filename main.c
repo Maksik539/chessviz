d18d3ba01c54df70578e5c59e5f5186f68112793
Maxim Merzlikin <merzlikinmax@mail.ru>
2020-03-07 20:48:52 +0700

    Corrected errors and added the file to gitignore

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <cmath>
 const double PI = 3.1415926535;
+static const size_t npos = -1;
 
 using namespace std;
 struct data_figure {
@@ -20,20 +21,21 @@ struct data_figure {
 } figure;
 void out_figure()
 {
-	double a,b,c;
-	for (int k = 0; k < figure.name.size();k++) {
-		cout<<k+1<<". "<< figure.name[k]<<"("<< figure.x[k]<<", "<< figure.y[k]<<", "<< figure.r[k]<<")"<<endl;
+	double a, b, c, h;
+	h = figure.name.size();
+	for (int k = 0; k < h; k++) {
+		cout << k + 1 << ". " << figure.name[k] << "(" << figure.x[k] << ", " << figure.y[k] << ", " << figure.r[k] << ")" << endl;
 		b = 2 * PI * figure.r[k];
-		cout<< "perimeter =" << b;
+		cout << "perimeter =" << b;
 		cout << endl;
 		c = PI * pow(figure.r[k], 2);
-		cout << "area =" << c<<endl;
+		cout << "area =" << c << endl;
 		cout << "intersects:" << endl;
-		for (int j = 0; j < figure.name.size(); j++) {
+		for (int j = 0; j < h; j++) {
 			if (k != j) {
-				a =sqrt(pow(figure.x[j] - figure.x[k], 2) + pow(figure.y[j] - figure.y[k], 2));
+				a = sqrt(pow(figure.x[j] - figure.x[k], 2) + pow(figure.y[j] - figure.y[k], 2));
 				if (figure.r[k] + figure.r[j] >= a)
-					cout <<j+1<<". "<< figure.name[j]<<endl;
+					cout << j + 1 << ". " << figure.name[j] << endl;
 			}
 		}
 		cout << endl;
@@ -42,8 +44,10 @@ void out_figure()
 int q = 0;
 int get_num(string com)
 {
+	double e;
+	e = com.length();
 	string num = "";
-	for (int i = q; i < com.length(); i++) {
+	for (int i = q; i < e; i++) {
 		if ((com[i] == ' ') || (com[i] == ',') || (com[i] == ')')) {
 			break;
 		}
@@ -57,10 +61,11 @@ int get_num(string com)
 }
 void get_figure()
 {
+	size_t npos = -1;
 	string com;
 	while (com.find("stop")) {
 		getline(cin, com);
-		if (com.find("circle")!= -1) {
+		if (com.find("circle")!=npos) {
 			figure.name.push_back("circle");
 			q = 7;
 			figure.x.push_back(get_num(com));
@@ -81,4 +86,3 @@ int main()
 	out_figure();
 
 }
-
