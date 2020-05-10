542ff259b3c8e7043ef1fee8963dd690ecba4b9e
Maxim Merzlikin <merzlikinmax@mail.ru>
2020-05-10 13:42:33 +0700

    Break the file into functions, add files to gitignore

---
+++ src/func.h
@@ -0,0 +1,24 @@
+
+#ifndef PERIMETER_H
+#define PERIMETER_H
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+
+float perimeters(float r);
+
+float areas(float r);
+
+void out_figure();
+
+int get_num(string com, int pos, int start_pos);
+
+void get_figure();
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include "func.h"
+
+const double PI = 3.1415926535;
+static const size_t npos = -1;
+
+using namespace std;
+
+int main()
+{
+	get_figure();
+	out_figure();
+	system("PAUSE");
+}
