2b823b239937f0c25846b7a4a529d3997c7154c4
Maxim Merzlikin <merzlikinmax@mail.ru>
2020-05-25 13:58:55 +0700

    Redid the makefile + travis ci for assembly

--- test/test_func.cpp
+++ test/test_func.cpp
@@ -1,21 +1,21 @@
 #define CATCH_CONFIG_MAIN
+#include "../src/func.h"
+#include "../thirdparty/catch.hpp"
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
 #include <string.h>
+#include <string>
 #include <vector>
-#include "../src/func.h"
-#include "catch.hpp"
 TEST_CASE("Perimeters are computed", "[perimeters]")
 {
     const float r = 1;
     const float result = perimeters(r);
     const float expected = 6.28;
-    REQUIRE( expected== result );
+    REQUIRE(expected == result);
 }
 
 TEST_CASE("Areas are computed", "[areas]")
@@ -23,30 +23,30 @@ TEST_CASE("Areas are computed", "[areas]")
     const float r = 1;
     const float result = areas(r);
     const float expected = 3.14;
-    REQUIRE( expected== result );
+    REQUIRE(expected == result);
 }
 
 TEST_CASE("Input check Circle", "[proverka]")
 {
-	int k=0,b=0;
-	const int d=1;
-	const int n=-1;
-	string com="circle";
-	string comm="square";
-    k=proverka(com);
-    b=proverka(comm);
-    REQUIRE(k==d);
-    REQUIRE(b==n);
+    int k = 0, b = 0;
+    const int d = 1;
+    const int n = -1;
+    string com = "circle";
+    string comm = "square";
+    k = proverka(com);
+    b = proverka(comm);
+    REQUIRE(k == d);
+    REQUIRE(b == n);
 }
 
 TEST_CASE("Taking parameters from input", "[get_num]")
 {
-	string a="circle(1, 2, 3)";
-	const int start_pos=7;
-	const int pos0=0;
-	const int pos1=1;
-	const int pos2=2;
-	REQUIRE(get_num(a, pos0, start_pos)==1);
-	REQUIRE(get_num(a, pos1, start_pos)==2);
-	REQUIRE(get_num(a, pos2, start_pos)==3);
+    string a = "circle(1, 2, 3)";
+    const int start_pos = 7;
+    const int pos0 = 0;
+    const int pos1 = 1;
+    const int pos2 = 2;
+    REQUIRE(get_num(a, pos0, start_pos) == 1);
+    REQUIRE(get_num(a, pos1, start_pos) == 2);
+    REQUIRE(get_num(a, pos2, start_pos) == 3);
 }
